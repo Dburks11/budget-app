53cac903e42802b44ed01e660d997fc4e0f74473
dburks11 <drb2006@outlook.com>
2025-05-22 08:11:46 -0500

    removed old table

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,18 @@
   import TransactionTable from "../components/TransactionTable.svelte";
 </script>
 
-<div class="flex">
-  <Topbar></Topbar>
-  <Sidebar></Sidebar>
-  <TransactionTable></TransactionTable>
+<div class="flex flex-col h-screen">
+  <Topbar />
+  <div class="flex flex-1 overflow-hidden">
+    <Sidebar />
+    <main class="flex flex-col flex-1 p-4 overflow-hidden mr-10">
+      <TransactionTable>
+        <input
+          type="text"
+          placeholder="Search"
+          class="input input-bordered input-sm w-full max-w-xs px-4 py-2 mr-3 rounded-md shadow-[0_2px_4px_rgba(0,0,0,0.1)] focus:outline-none"
+        />
+      </TransactionTable>
+    </main>
+  </div>
 </div>
